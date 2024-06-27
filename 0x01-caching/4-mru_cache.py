509c11ed7af2945b02081ef8c21b3154be38222f
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-27 03:22:04 +0300

    MRU

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -31,7 +31,7 @@ class MRUCache(BaseCaching):
         if key in self.cache_data:
             self.cache_data.move_to_end(key)
         elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            popped = self.cache_data.popitem(False)
+            popped = self.cache_data.popitem()
             print("DISCARD: {}".format(popped[0]))
         self.cache_data[key] = item
 
